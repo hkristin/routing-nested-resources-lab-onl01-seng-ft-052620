e79e715b3e014c56338cbff29787272e6fcf16e1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-11 16:04:18 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,8 +13,15 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
-  end
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else
+      @song = Song.find(params[:id])
+    end
 
   def new
     @song = Song.new
