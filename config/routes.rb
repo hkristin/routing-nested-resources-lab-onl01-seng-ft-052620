06533145c50c5f63ef815a32fe135dc457bfd14e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-11 16:02:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
     resources :songs, only: [:show, :index]
   end
-  
+
   resources :songs,  only: [:index, :show, :new, :create, :edit, :update]
 
   root 'songs#index'
